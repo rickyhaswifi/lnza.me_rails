e13381680327ac93acde20909227bff3f56c8204
Ricardo Bautista <37841371+rickyhaswifi@users.noreply.github.com>
2019-09-03 11:05:19 -0600

    Delete aws.rb

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-     region: ‘us-east-1’,
-     credentials: Aws::Credentials.new(ENV[‘AKIAJNKWO5WYV45FYBUA’], ENV[‘KAkVvGYjfh+hYwANxKDfpM0XSw6xbxK9O6KUtJyY’]),
-    })
-    
-    S3_BUCKET = Aws::S3::Resource.new.bucket(ENV[‘lnza.me’])
